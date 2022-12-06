7741b55e44393a79bbde68031c46f90608a6f65c
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-05 22:47:30 -0800

    Add files via upload

---
+++ if.c
@@ -0,0 +1,16 @@
+// program to display a number if it is negative
+#include<stdio.h>
+void main()
+{
+int number ;
+ printf("Enter an interger:");
+scanf("%d",&number);
+
+
+// true if number is less than 0
+if (number< 0)
+{
+ printf("%d\n",number);
+}
+printf(" the if statement is esay.");
+} 
